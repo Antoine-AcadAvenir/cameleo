e11510bba848ed4f6ecec17d112e3fd36a5fa5ee
DataHorizon <antoine.brnl1212@gmail.com>
2025-07-31 00:10:09 +0200

    ton message de commit

--- src/components/nowts/typography.tsx
+++ src/components/nowts/typography.tsx
@@ -3,10 +3,9 @@ import { cn } from "@/lib/utils";
 import type { VariantProps } from "class-variance-authority";
 import { cva } from "class-variance-authority";
 import type { ComponentPropsWithRef, ElementType, ForwardedRef } from "react";
-
 import React, { forwardRef } from "react";
 
-// Source : https://www.totaltypescript.com/pass-component-as-prop-react
+// Fix for forwardRef with generics
 type FixedForwardRef = <T, P = {}>(
   render: (props: P, ref: React.Ref<T>) => React.ReactNode,
 ) => (props: P & React.RefAttributes<T>) => React.ReactNode;
@@ -57,65 +56,45 @@ const defaultElementMapping = {
 
 type TypographyVariant = keyof typeof defaultElementMapping;
 
-interface TypographyProps {
-  variant?: TypographyVariant;
-  as?: ElementType;
+interface TypographyProps<TAs extends ElementType = ElementType>
+  extends TypographyCvaProps {
+  as?: TAs;
   className?: string;
   children?: React.ReactNode;
 }
 
-type ElementMapping = typeof defaultElementMapping;
-
-type ElementTypeForVariant<TVariant extends keyof ElementMapping> =
-  ElementMapping[TVariant];
-
 /**
- * The Typography component is useful to add Text to your page
+ * The Typography component is useful to add Text to your page.
  *
- * Usage :
- *
- * ```tsx
- * <Typography variant="h1">Hello World</Typography>
- * <Typography variant="h2" as="a" href="#">Hello World</Typography>
- * <Typography variant="large" as={Link} href="#">Hello World</Typography>
- * ```
- *
- * You can use the `as` prop to define the element type of the component
- * `as` can be a string or a component
- *
- * @param params The parameters of the component
- * @param ref The ref of the element. Untyped because it's a generic
- * @returns
+ * Usage:
+ * <Typography variant="h1">Title</Typography>
+ * <Typography variant="h2" as="a" href="#">Link</Typography>
  */
 const InnerTypography = <
-  TAs extends ElementType,
-  TVariant extends TypographyCvaProps["variant"] = "default",
+  TAs extends ElementType = ElementType,
+  TVariant extends TypographyVariant = "default",
 >(
   {
     variant = "default",
     className,
     as,
     ...props
-  }: {
-    as?: TAs;
-    variant?: TVariant;
-  } & DistributiveOmit<
-    ComponentPropsWithRef<
-      ElementType extends TAs
-        ? ElementTypeForVariant<NonNullable<TVariant>>
-        : TAs
+  }: TypographyProps<TAs> &
+    DistributiveOmit<
+      ComponentPropsWithRef<TAs>,
+      "as" | "className" | "children" | "variant"
     >,
-    "as"
-  >,
   ref: ForwardedRef<any>,
 ) => {
-  const Comp = as ?? defaultElementMapping[variant ?? "default"]; as keyof typeof defaultElementMapping] ?? "p";
+  const Comp: ElementType =
+    as ?? defaultElementMapping[variant ?? "default"] ?? "p";
+
   return (
     <Comp
       {...props}
       className={cn(typographyVariants({ variant }), className)}
       ref={ref}
-    ></Comp>
+    />
   );
 };
 
